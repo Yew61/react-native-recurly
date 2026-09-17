4ced73d289f915aa52ef6569c1d257b817bf9029
THE-BEAST\adamh <adam@beachgum.com>
2026-09-17 11:12:40 +1000

    all code commited

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import "@/global.css"
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-background">
+            <Text className="text-xl font-bold text-success">
+                Welcome to Nativewind!
+            </Text>
+            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+                Go To Onboarding
+            </Link>
+            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">
+                Go To Sign In
+            </Link>
+            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
+                Go To Sign Up
+            </Link>
+            <Link href="/subscriptions/spotify">
+                Spotify Subscriptions
+            </Link>
+            <Link
+                href={{
+                    pathname: "/subscriptions/[id]",
+                    params: {id:"claude"},
+                }}
+            >
+                Claude Max Subscriptions
+            </Link>
+        </View>
+    );
+}
